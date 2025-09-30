23768c56ef107b4d1a7b14fce7663fe47590001f
DrkNowak <dariusznowak194@gmail.com>
2025-09-30 22:58:59 +0200

    database util functions

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -5,17 +5,31 @@ const db = new sqlite3.Database('user_database.db');
 function initDB() {
   db.serialize(() => {
     db.run(`CREATE TABLE IF NOT EXISTS users (
-    _id PRIMARY KEY,
-    username
-  )`);
+      _id PRIMARY KEY,
+      username
+    )`);
 
-    // 3. Wstawianie danych
-    const stmt = db.prepare('INSERT INTO users (_id, username) VALUES (?, ?)');
-    stmt.run(uuidv4(), 'Jan');
-    stmt.run(uuidv4(), 'Anna2');
-    stmt.finalize();
+    return db;
   });
+}
 
-  // 5. Zamknięcie bazy
-  // db.close();
+export function checkIfUserIsInDB(username: string, callback: (err: Error | null, exists: boolean | null) => void) {
+  db.get('SELECT * FROM users WHERE username = ?', [username], (err: Error | null, row: any) => {
+    if (err) {
+      return callback(err, null);
+    }
+    callback(null, !!row);
+  });
+}
+
+function insertNewUser(username: string) {
+  const stmt = db.prepare('INSERT INTO users (_id, username) VALUES (?, ?)');
+
+  stmt.run(uuidv4(), username);
+
+  stmt.finalize();
+}
+
+function killDBConnection() {
+  db.close();
 }
