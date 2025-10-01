4718888248df323cafb45b4b93b64c8fe2ea84a6
DrkNowak <dariusznowak194@gmail.com>
2025-10-01 13:17:31 +0200

    feat: models

---
+++ src/models/user.ts
@@ -0,0 +1,27 @@
+type User = {
+  _id: string;
+  username: string;
+};
+
+type Exercise = {
+  username: string;
+  description: string;
+  duration: string;
+  date: string;
+  _id: string;
+};
+
+type Task = {
+  description: string;
+  duration: number;
+  date: string;
+};
+
+type Log = {
+  username: string;
+  _id: string;
+  count: number;
+  log: Task[];
+};
+
+export { User, Log, Exercise };
