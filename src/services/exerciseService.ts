e57ebc8294ead21e59f7f77852ae727028b26a52
DrkNowak <dariusznowak194@gmail.com>
2025-10-04 23:30:52 +0200

    feat: basic validation

--- src/services/exerciseService.ts
+++ src/services/exerciseService.ts
@@ -1,7 +1,22 @@
 import { Exercise } from '../models/models.js';
 
-export const createExercise = async (userId, description, duration, date) => {
-  const newExercise = { userId, description, duration, date };
+export const createExercise = async ({ _id, description, duration, date }: Exercise) => {
+  const newExercise = { _id, description, duration, date };
+
+  switch (true) {
+    case !newExercise._id:
+      throw new Error('User ID is required');
+
+    case !newExercise.description || typeof newExercise.description !== 'string':
+      throw new Error('Description is required and must be a string');
+
+    case !newExercise.duration || isNaN(Number(newExercise.duration)):
+      throw new Error('Duration is required and must be a number');
+
+    case newExercise.date && isNaN(Date.parse(newExercise.date)):
+      throw new Error('Date must be in YYYY-MM-DD format');
+  }
+
   return newExercise;
 };
 
